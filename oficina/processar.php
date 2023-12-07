a57b37c7c19e0372bad08b1317ee18e5699acf9b
Pablo <152925963+SEngPablo@users.noreply.github.com>
2023-12-07 20:51:54 -0300

    Add files via upload

--- oficina/processar.php
+++ oficina/processar.php
@@ -14,11 +14,27 @@
 $pneu = $_POST['pneu'];
 $oleo = $_POST['oleo'];
 $vela = $_POST['vela'];
+$valortotal = 0.00;
+$qtdtotal = 0;
+$taxrate = 0.27;
+
+define('PRECOPNEU',100);
+define('PRECOOLEO',10);
+define('PRECOVELA',4);
+
+$valortotal = $pneu*PRECOPNEU + $oleo*PRECOOLEO + $vela*PRECOVELA;
 
 echo '<p>Pedido processado ás: '.date('H:i, d/m/y').'</p>';
 echo $pneu.' pneus <br>';
 echo $oleo.' galões <br>';
 echo $vela.' velas <br>';
+$qtdtotal = $pneu + $oleo + $vela;
+echo 'A quantidade total é: '.$qtdtotal.'<br>';
+echo 'O subtotal a pagar é: R$'.number_format($valortotal, 2,',','.').'<br>';
+
+echo 'O valor total é: ';
+echo $valortotal * (1 + $taxrate).'<br>';
+echo 'Total de imposto: '.$valortotal * $taxrate;
 
 ?>
 
